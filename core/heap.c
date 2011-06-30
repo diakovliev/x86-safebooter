a54de0b22bd06a9ecce835d057016382c677df64
Dmytro Iakovliev <D.Iakovliev@diakovliev.(none)>
2011-06-30 16:32:14 +0300

    Added memory manager; Added GMP library

--- core/heap.c
+++ core/heap.c
@@ -88,12 +88,12 @@ void *malloc(size_t size)
 	if (node) {
 		NODE_CTL(node)->busy = 1;
 
-		printf("+++ Allocated %d bytes at %p\n\r", size, node->start);
+//		printf("+++ Allocated %d bytes at %p\n\r", size, node->start);
 
 		return node->start;	
 	}
 
-	printf("!!! Allocion failed !!!\n\r");
+	printf("!!! Allocation failed !!!\n\r");
 
 	return 0;
 }
@@ -104,10 +104,12 @@ void free(void *ptr)
 	while (current_node->start != ptr && current_node < heap_nodes + heap_nodes_count) {
 		++current_node;
 	}
-	if (!current_node)
+	if (!current_node) {
+		printf("!!! Was tried to free unknown node at %p !!!\n\r", ptr);
 		return;
+	}
 
-	printf("--- Relesed %d bytes at %p\n\r", NODE_CTL(current_node)->size, current_node->start);
+//	printf("--- Relesed %d bytes at %p\n\r", NODE_CTL(current_node)->size, current_node->start);
 	NODE_CTL(current_node)->busy = 0;
 }
 
