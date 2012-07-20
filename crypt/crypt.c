dcbb3066cd70f6127f7c848338448cba9dac4471
Dmytro Iakovliev <D.Iakovliev@adbglobal.com>
2012-07-20 16:42:02 +0300

    Various blowfish encryption modes where implemented

--- crypt/crypt.c
+++ crypt/crypt.c
@@ -4,8 +4,11 @@
 #include "blowfish.h"
 #include "crypt.h"
 
-//#define CONFIG_BLOWFISH_MODE_ECB
-#define CONFIG_BLOWFISH_MODE_CBC
+/*#define CONFIG_BLOWFISH_MODE_ECB /* not recomended to use */
+#define CONFIG_BLOWFISH_MODE_CTR
+/*#define CONFIG_BLOWFISH_MODE_CBC*/
+/*#define CONFIG_BLOWFISH_MODE_CFB*/
+/*#define CONFIG_BLOWFISH_MODE_OFB*/
 
 extern uint8_t blowfish_key[];
 extern uint32_t blowfish_key_size;
@@ -18,6 +21,7 @@ void blowfish_reset(void) {
 }
 
 #if defined(CONFIG_BLOWFISH_MODE_ECB)
+#warning "Enabled ECB encryption mode, we are strongly recommend do not use this mode."
 
 void blowfish_encrypt_memory(void* buffer, uint32_t size) {
 	unsigned long *array = (unsigned long *)buffer;	
@@ -30,6 +34,36 @@ void blowfish_decrypt_memory(void* buffer, uint32_t size) {
 	unsigned long *array = (unsigned long *)buffer;	
 	for ( ; array+1 < (unsigned long *)(buffer+size+size%2); array += 2) {
 		Blowfish_Decrypt(&blowfish_context, array, (array+1));
+	}
+}
+
+#elif defined(CONFIG_BLOWFISH_MODE_CTR)
+
+void blowfish_encrypt_memory(void* buffer, uint32_t size) {
+	unsigned long ctr = 0;
+	unsigned long a_0 = 0;
+	unsigned long a_1 = 0;
+	unsigned long *array = (unsigned long *)buffer;	
+	for ( ; array+1 < (unsigned long *)(buffer+size+size%2); array += 2) {
+		a_0 = ctr++ ^ *array;
+		a_1 = ctr++ ^ *(array + 1);
+		Blowfish_Encrypt(&blowfish_context, &a_0, &a_1);
+		*array = a_0;
+		*(array + 1) = a_1;
+	}
+}
+
+void blowfish_decrypt_memory(void* buffer, uint32_t size) {
+	unsigned long ctr = 0;
+	unsigned long a_0 = 0;
+	unsigned long a_1 = 0;
+	unsigned long *array = (unsigned long *)buffer;	
+	for ( ; array+1 < (unsigned long *)(buffer+size+size%2); array += 2) {
+		a_0 = *array;
+		a_1 = *(array + 1);
+		Blowfish_Decrypt(&blowfish_context, &a_0, &a_1);
+		*array = a_0 ^ ctr++;
+		*(array + 1) = a_1 ^ ctr++;
 	}
 }
 
@@ -80,6 +114,106 @@ void blowfish_decrypt_memory(void* buffer, uint32_t size) {
 
 		*array = a_0;
 		*(array + 1) = a_1;
+	}
+}
+
+#elif defined(CONFIG_BLOWFISH_MODE_CFB)
+
+void blowfish_encrypt_memory(void* buffer, uint32_t size) {
+	unsigned long *array = (unsigned long *)buffer;
+	unsigned long a_0 = 0;
+	unsigned long a_1 = 0;
+	unsigned long pa_0 = 0;
+	unsigned long pa_1 = 0;
+	for ( ; array+1 < (unsigned long *)(buffer+size+size%2); array += 2) {
+
+		a_0 = pa_0;
+		a_1 = pa_1;
+
+		Blowfish_Encrypt(&blowfish_context, &a_0, &a_1);
+
+		a_0 = a_0 ^ *array;
+		a_1 = a_1 ^ *(array + 1);
+
+		*array = a_0;
+		*(array + 1) = a_1;
+
+		pa_0 = a_0;
+		pa_1 = a_1;
+	}
+}
+
+void blowfish_decrypt_memory(void* buffer, uint32_t size) {
+	unsigned long *array = (unsigned long *)buffer;	
+	unsigned long a_0 = 0;
+	unsigned long a_1 = 0;
+	unsigned long pa_0 = 0;
+	unsigned long pa_1 = 0;
+	for ( ; array+1 < (unsigned long *)(buffer+size+size%2); array += 2) {
+
+		Blowfish_Encrypt(&blowfish_context, &a_0, &a_1);
+
+		pa_0 = *array;
+		pa_1 = *(array + 1);
+
+		a_0 = a_0 ^ *array;
+		a_1 = a_1 ^ *(array + 1);
+
+		*array = a_0;
+		*(array + 1) = a_1;
+
+		a_0 = pa_0;
+		a_1 = pa_1;
+	}
+}
+
+#elif defined(CONFIG_BLOWFISH_MODE_OFB)
+
+void blowfish_encrypt_memory(void* buffer, uint32_t size) {
+	unsigned long *array = (unsigned long *)buffer;
+	unsigned long a_0 = 0;
+	unsigned long a_1 = 0;
+	unsigned long pa_0 = 0;
+	unsigned long pa_1 = 0;
+	for ( ; array+1 < (unsigned long *)(buffer+size+size%2); array += 2) {
+
+		a_0 = pa_0;
+		a_1 = pa_1;
+
+		Blowfish_Encrypt(&blowfish_context, &a_0, &a_1);
+
+		pa_0 = a_0;
+		pa_1 = a_1;
+
+		a_0 = a_0 ^ *array;
+		a_1 = a_1 ^ *(array + 1);
+
+		*array = a_0;
+		*(array + 1) = a_1;
+	}
+}
+
+void blowfish_decrypt_memory(void* buffer, uint32_t size) {
+	unsigned long *array = (unsigned long *)buffer;	
+	unsigned long a_0 = 0;
+	unsigned long a_1 = 0;
+	unsigned long pa_0 = 0;
+	unsigned long pa_1 = 0;
+	for ( ; array+1 < (unsigned long *)(buffer+size+size%2); array += 2) {
+
+		Blowfish_Encrypt(&blowfish_context, &a_0, &a_1);
+
+		pa_0 = a_0;
+		pa_1 = a_1;
+
+		a_0 = a_0 ^ *array;
+		a_1 = a_1 ^ *(array + 1);
+
+		*array = a_0;
+		*(array + 1) = a_1;
+
+		a_0 = pa_0;
+		a_1 = pa_1;
 	}
 }
 
