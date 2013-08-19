b5cdb2b8eaa70aa9f21c86a267f58f9745edf94c
Dmytro Iakovliev <D.Iakovliev@adbglobal.com>
2013-08-19 17:28:09 +0300

    Corrected TO_LINK handling

--- mk/functions.mk
+++ mk/functions.mk
@@ -1,5 +1,7 @@
 # ---- arch ----
 GCC				= gcc
+LD				= ld
+OBJCOPY			= objcopy
 GCCARGS			= -c $(CONFIG-DBG-$(CONFIG_DBG)) -m32 -march=i386 -nostdlib -fno-builtin $(DEFINES) $(INCLUDES)
 
 RED=printf "\x1b[31m"
@@ -18,15 +20,21 @@ GCC_CMD	= $(YELLOW) && printf "[CC]" && $(NORMAL) && printf " %s " $1 && \
 	$(GCC) $(GCCARGS) $1 && \
 	$(GREEN) && printf "OK\n" && $(NORMAL)
 
-LD_IMG_CMD	= $(CYAN) && printf "[LD]" && $(NORMAL) && printf " %s " $@ && \
-	ld -A i386 -melf_i386 -N -static -Ttext $2 -Map=$@.map $1 -o$@.elf $(SUPPRESS_LD_OUTPUT) && \
-	objcopy --only-keep-debug $@.elf $@.dbg && \
-	objcopy --strip-debug $@.elf && \
-	objcopy -O binary $@.elf $@ && \
+ifneq ($(TO_LINK),)
+LD_LIB_CMD = $(BLUE) && printf "[LD]" && $(NORMAL) && printf " %s " $@ && \
+	$(LD) --whole-archive -r $1 -o$@.lib $(SUPPRESS_LD_OUTPUT) && \
+	$(GREEN) && printf "OK\n" && $(NORMAL) && \
+	if [ ! -z "$(TO_LINK)" ]; then echo $(CDIR)/$@.lib >> $(TO_LINK); fi
+else
+LD_LIB_CMD = $(BLUE) && printf "[LD]" && $(NORMAL) && printf " %s " $@ && \
+	$(LD) --whole-archive -r $1 -o$@.lib $(SUPPRESS_LD_OUTPUT) && \
 	$(GREEN) && printf "OK\n" && $(NORMAL)
+endif
 
-LD_LIB_CMD	= $(BLUE) && printf "[LD]" && $(NORMAL) && printf " %s " $@ && \
-	ld --whole-archive -r $1 -o$@.lib $(SUPPRESS_LD_OUTPUT) && \
-	if [ ! -z "$(TO_LINK)" ]; then echo $(CDIR)/$@.lib >> $(TO_LINK); fi && \
+LD_IMG_CMD = $(CYAN) && printf "[LD]" && $(NORMAL) && printf " %s " $@ && \
+	$(LD) -A i386 -melf_i386 -N -static -Ttext $2 -Map=$@.map $1 -o$@.elf $(SUPPRESS_LD_OUTPUT) && \
+	$(OBJCOPY) --only-keep-debug $@.elf $@.dbg && \
+	$(OBJCOPY) --strip-debug $@.elf && \
+	$(OBJCOPY) -O binary $@.elf $@ && \
 	$(GREEN) && printf "OK\n" && $(NORMAL)
 
