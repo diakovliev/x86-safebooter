fcecb2bc7abcde28e69e3ce7ec1905656a7fb5a5
Dmytro Iakovliev <dmytro.iakovliev@dev.zodiac.tv>
2014-11-04 13:00:20 +0200

    fixed compilation under ubuntu x86-54

--- mk/functions.mk
+++ mk/functions.mk
@@ -1,10 +1,11 @@
 # ---- arch ----
-GCC				= gcc
-#GCC				= clang
-LD				= ld
-AR				= ar
+GCC			= gcc
+#GCC			= clang
+LD			= ld
+AR			= ar
 OBJCOPY			= objcopy
 GCCARGS			= -c $(CONFIG-DBG-$(CONFIG_DBG)) -m32 -march=i386 -nostdlib -fno-builtin $(DEFINES) $(INCLUDES)
+LDARGS			= -A i386 -melf_i386 -telf_i386 
 
 #SUPPRESS_LD_OUTPUT= > /dev/null 2>/dev/null
 #SUPPRESS_LD_OUTPUT=
@@ -18,13 +19,13 @@ endef
 
 define LD_LIB_CMD 
 	$(BLUE) && printf " [LD]" && $(NORMAL) && printf " %s " $@ && \
-	$(LD) --whole-archive -r -Map=$@.map $1 -o$@.lib $(SUPPRESS_LD_OUTPUT) && \
+	$(LD) $(LDARGS) --whole-archive -r -Map=$@.map $1 -o$@.lib $(SUPPRESS_LD_OUTPUT) && \
 	$(GREEN) && printf "OK\n" && $(NORMAL)
 endef
 
 define LD_IMG_CMD 
 	$(CYAN) && printf " [LD]" && $(NORMAL) && printf " %s " $@ && \
-	$(LD) -A i386 -melf_i386 -N -static -Ttext $2 -Map=$@.map $1 -o$@.elf $(SUPPRESS_LD_OUTPUT) && \
+	$(LD) $(LDARGS) -N -static -Ttext $2 -Map=$@.map $1 -o$@.elf $(SUPPRESS_LD_OUTPUT) && \
 	$(OBJCOPY) --only-keep-debug $@.elf $@.dbg && \
 	$(OBJCOPY) --strip-debug $@.elf && \
 	$(OBJCOPY) -O binary $@.elf $@ && \
